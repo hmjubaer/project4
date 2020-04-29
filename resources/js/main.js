46bb3b4be653f3a6312221a01b4b2a84d80d39d2
hmjubaer <hmjubaer50@gmail.com>
2020-04-29 15:02:04 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    
+   //sticky menu
+    $(".js--services-section").waypoint(function(direction){
+        if (direction=="down"){
+            $("nav").addClass("sticky");
+            } else{
+            $("nav").removeClass("sticky");
+            }
+    });
+    // ACTIVE LINK
+    
+    $("nav ul li a").click(function(){
+        $("nav ul li a").removeClass("active");
+        $(this).addClass("active");
+    });
+        $("nav a img .logo").click(function(){
+        $("nav ul li a").removeClass("active");
+        $("nav ul li:first-child a").addClass("active");
+    });
+    
+
+    //mixituo (portfolio section)
+    var mixer = mixitup('.container');
+});
+function openNav(){
+    document.getElementById("myNav").style.width="100%";
+}
+function closeNav(){
+    document.getElementById("myNav").style.width="0%";
+}
